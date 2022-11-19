27665b51d45c73d925be7e48c46a2fba6c809318
Nguyen Van Tan <112942045+VanTan1405@users.noreply.github.com>
2022-11-19 23:36:01 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Thầy Cô';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-11';
+const titleCard = 'Tặng Người lái đò';
+const contentCard = 'Nhân Ngày Nhà giáo Việt Nam 20-11, xin chúc cho tất cả các thầy cô giáo trên mọi miền đất nước sức khỏe, hạnh phúc và thật thành công trong sự nghiệp trồng người.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'images/nhagiaoVN.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
